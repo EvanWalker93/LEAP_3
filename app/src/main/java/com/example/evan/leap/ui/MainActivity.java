ade29342d69356f0ac03fa4c72fe2ae0dfc0944d
Evan Walker <scdv@iup.edu>
2017-04-28 18:51:15 -0400

    Storing items with SharedPreferences should work properly now.

--- app/src/main/java/com/example/evan/leap/ui/MainActivity.java
+++ app/src/main/java/com/example/evan/leap/ui/MainActivity.java
@@ -86,6 +86,7 @@ public class MainActivity extends AppCompatActivity {
 
         String path = QuizItems.toString().replace("[", "").replace("]", "").trim();
 
+
         if (path == ""){
             return;
         }
